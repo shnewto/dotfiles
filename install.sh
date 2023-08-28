6ab65ad0ad4228678db2e46f7f0f37bc731852ff
shnewto <github@shnewto.dev>
2023-08-28 15:19:28 -0600

    source cargo env after install

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ git clone https://github.com/shnewto/space.git "$HOME/cli/space"
 
 # rust 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source "$HOME/.cargo/env"
 
 # ripgrep
 cargo install ripgrep
